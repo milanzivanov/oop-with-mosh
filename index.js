9f2b3b60a424a7c2e420dd65558bc74b86fcf879
milan.zivanov <milan.zivanov@gmail.com>
2018-05-21 12:36:54 +0200

    object literals

---
+++ index.js
@@ -0,0 +1,15 @@
+// object literal
+const circle = {
+    // properties
+    radius: 3,
+    location: {
+        x: 2,
+        y: 3
+    },
+    // method
+    draw() {
+        console.log("Drow");
+    }
+}
+
+circle.draw(); 
