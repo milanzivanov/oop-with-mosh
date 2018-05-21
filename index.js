d01a07b8df315e159775939b75a7c7a57789564b
milan.zivanov <milan.zivanov@gmail.com>
2018-05-22 00:02:19 +0200

    Private Properties and methods

--- index.js
+++ index.js
@@ -1,21 +1,47 @@
-// Adding/Removing Properties
+// Enumereting Properties
+
+// function Circle(radius) {
+//     this.radius = radius;
+//     this.drow = function() {
+//         console.log('Drow!!!'); 
+        
+//     }
+// }
+
+// const circle = new Circle(11);
+
+// for (let key in circle) {
+//     if (typeof circle[key] !== 'function') {
+//         console.log(key, circle[key]);
+//     }
+// }
+
+// const keys = Object.keys(circle);
+// console.log(keys);
+
+// if ('radius' in circle) {
+//     console.log("Circle has a radius");
+    
+// }
+
+// Private Properties and methods
 
 function Circle(radius) {
     this.radius = radius;
-    this.drow = function() {
-        console.log('Drow!!!'); 
-        
+
+    let defaultLocation = { x: 0, y: 0};
+    let computeOptimumLocation = function(factor) {
+        // magic
     }
+
+    this.drow = function() {
+        computeOptimumLocation(0.1);
+        // defaultLocation
+        // this.radius
+
+        console.log('Drow!!!');
+    };
 }
 
-const circle = new Circle(11);
-
-circle.location = { x: 3 };
-// same
-// bracket notetion
-// circle['location'] = { x: 3 };
-
-delete circle.location;
-// delete circle['location'];
-
-console.log(circle);
+const circle = new Circle(10);
+circle.drow();
