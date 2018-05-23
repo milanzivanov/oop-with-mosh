958433a90f383605576940948c8b73aa57932c08
milan.zivanov <milan.zivanov@gmail.com>
2018-05-23 16:47:25 +0200

    Promise.all()

--- index.js
+++ index.js
@@ -17,23 +17,9 @@ const preMovie = async () => {
 
     // we can use await inside async function
     let ticket = await promiseWifeBringingTicks;
+    let [popcorn, butter] = await Promise.all([getPopcorn, addButter]);
 
-    console.log(`Wife: I have the ${ticket}`);
-    console.log(`Husband: We should go in`);
-    console.log(`Wife: No I am hungry`);
-
-    let popcorn = await getPopcorn;
-
-    console.log(`Husband: here is ${popcorn}`);
-    console.log(`Husband:we should go in now`);
-    console.log(`Wife: "I dont like popcorn without butter!"`);
-    
-    let butter = await addButter;
-    console.log(`added ${butter}`);
-
-    console.log(`Husband:Anything else darling`);
-    console.log(`Wife: lets go we are going to miss the preivew`);
-    console.log(`Husband: thanks for the reminder *grin*`);
+    console.log(`${popcorn}, ${butter}`);
     
     return ticket;
 }
