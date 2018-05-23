5fc817092ba87937ca65284bdaaee2ef2516bff8
milan.zivanov <milan.zivanov@gmail.com>
2018-05-23 15:58:30 +0200

    async expresion function simple

--- index.js
+++ index.js
@@ -1,40 +1,9 @@
-function Stopwatch() {
-    let startTime, endTime, running, duration = 0;
+console.log(`Person 1 shows ticket`);
+console.log(`Person 2 shows ticket`);
 
-    this.start = function() {
-        if(running) {
-            throw new Error('Stopwatch has already started.');
-        }
-        running = true;
+const preMovie = async () => 'preMovie';
 
-        startTime = new Date(); 
-    };
+preMovie().then((m) => console.log(m));
 
-    this.stop = function() {
-        if(!running) {
-            throw new Error('Stopwatch has not started.');
-        }
-        running = false;
-
-        endTime = new Date();
-
-        const seconds = (endTime.getTime() - startTime.getTime()) / 1000;
-        duration += seconds;
-    }
-
-    this.reset = function() { 
-        startTime = null;
-        endTime = null;
-        running = false;
-        duration = 0;
-    }
-
-    Object.defineProperty(this, 'duration', {
-        get: function() {
-            return duration;
-        }
-    });
-}
-
-let sw = new Stopwatch();
-
+console.log(`Person 4 shows ticket`);
+console.log(`Person 5 shows ticket`);
