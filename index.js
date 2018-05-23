2974f5d867af59175a8f319ab803563259cba038
milan.zivanov <milan.zivanov@gmail.com>
2018-05-23 16:23:31 +0200

    async with await simple

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 console.log(`Person 1 shows ticket`);
 console.log(`Person 2 shows ticket`);
 
-const preMovie = async () => 'preMovie';
+const preMovie = async () => {
+    // async returns Promise
+    const promiseWifeBringingTicks = new Promise((resolve, reject) => {
+        setTimeout(() => resolve('ticket is hire after 3sec!!!'), 3000);
+    });
+    // we can use await inside async function
+    let ticket = await promiseWifeBringingTicks;
+
+    return ticket;
+}
 
 preMovie().then((m) => console.log(m));
 
