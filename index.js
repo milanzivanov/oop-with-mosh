8df87d14b404ffaadea08dbc06486fe913450f30
milan.zivanov <milan.zivanov@gmail.com>
2018-05-24 11:46:17 +0200

    Prototype

--- index.js
+++ index.js
@@ -4,18 +4,27 @@ function User (email, name) {
     this.email = email;
     this.name = name;
     this.online = false;
-    this.login = () => {
-        console.log(this.email, 'just logged in');
-    }
+}
+
+User.prototype.login = function() {
+    this.online = true;
+    console.log(this.email, 'just logged in');
+}
+
+User.prototype.logout = function() {
+    this.online = false;
+    console.log(this.email, 'just logged out');
 }
 
 let userOne = new User('milanodituti@m.com', 'Mlano');
 let userTwo = new User('ivandituti@m.com', 'Ivano');
 
 // The 'new' keyword
-// Creates a nnew empty object {}
+// Creates a new empty object {}
 // Sets the value of 'this' to be the new empty object
 // Calls the constructor method
 
 console.log(userOne);
 console.log(userTwo);
+userTwo.login();
+userOne.logout();
