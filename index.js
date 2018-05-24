380c557f8fd689c7585d9f8906fe4629fb98720c
milan.zivanov <milan.zivanov@gmail.com>
2018-05-24 12:09:00 +0200

    Prototype Inheritance

--- index.js
+++ index.js
@@ -16,15 +16,31 @@ User.prototype.logout = function() {
     console.log(this.email, 'just logged out');
 }
 
+function Admin(...args) {
+    console.log(args);
+    User.apply(this, args);
+    this.role = 'Super user';
+}
+
+// inherit User
+Admin.prototype = Object.create(User.prototype);
+
+Admin.prototype.deliteUser = function(u) {
+    users = users.filter(user => {
+        return user.email !== u.email;
+    });
+};
+
 let userOne = new User('milanodituti@m.com', 'Mlano');
 let userTwo = new User('ivandituti@m.com', 'Ivano');
+let admin = new Admin('learning@m.com', 'Kiki');
+
+let users = [userOne, userTwo, admin];
 
 // The 'new' keyword
 // Creates a new empty object {}
 // Sets the value of 'this' to be the new empty object
 // Calls the constructor method
 
-console.log(userOne);
-console.log(userTwo);
-userTwo.login();
-userOne.logout();
+console.log(admin);
+
